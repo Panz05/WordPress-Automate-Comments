18ad3fb5b52383df9c1eb0dec7637ca96bac7541
Panz05 <32978609+Panz05@users.noreply.github.com>
2022-08-03 21:38:23 +0930

    Update Automate_Comments.py
    New and Improved...
    Updated to work with Selenium 4.x

--- Automate_Comments.py
+++ Automate_Comments.py
@@ -1,37 +1,44 @@
 ##################################################
 ### Script To Copy Contents Of File To WebPage ###
-#### Version 0.1 - Creation Date - 04/05/2018 ####
+#### Version 0.2 - Creation Date - 03/08/2022 ####
 ############# Author - Imp0st3r ##################
 ##################################################
 
 # Python modules to import
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pyperclip
 
-msg = open('Path To File', 'r').read() # Enter the path to the file you wish to read from
+msg = open('comments.txt', 'r').read() # Enter the path to the file you wish to read from
 
-driver = webdriver.Chrome('Path To chromedriver.exe') # Enter the path to the ChromeDrive.exe file
-driver.get('My WebSite') # Enter the website you wish to copy your data to
+options = Options()
+options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+options.add_argument("start-maximized")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get('https://my/web/target/') # Enter the website you wish to copy your data to
 
-usr_box = driver.find_element_by_id('author') # Search for the element on the web page called 'author'
-usr_box.send_keys('MyNameHere') # Enter the name you wish to use
+search_box = driver.find_element(By.NAME, 'author') # Search for the element on the web page called 'author'
+search_box.send_keys('Enter a name') # Enter the name you wish to use
 
 sleep (2)
 
-eml_box = driver.find_element_by_id('email') # Search for the element on the web page called 'email'
+eml_box = driver.find_element(By.NAME, 'email') # Search for the element on the web page called 'email'
 eml_box.send_keys('MyName@MyDomain.com') # Enter the email address you wish to use
 
 sleep (2)
 
-msg_box = driver.find_element_by_id('comment') # Search for the element on the web page called 'comment'
+msg_box = driver.find_element(By.NAME, 'comment') # Search for the element on the web page called 'comment'
 pyperclip.copy(msg) # Using Payperclip copy the contents of the file opened above to the clipboard
 msg_box.send_keys(Keys.CONTROL, 'v') # Using Selenium paste the contents from the clip board to the web site
 
 sleep (3)
 
-post_comment = driver.find_element_by_id('submit') # Search for the element on the web page called submit
-post_comment.click() # Submit post
+post_comment = driver.find_element(By.NAME, 'submit') # Search for the element on the web page called submit
+post_comment.submit() # Submit post
 
 driver.quit() # Quit program
